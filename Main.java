6017dc108bc5fdfc0ac844bba05664687a527bfb
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2021-12-10 14:57:20 +0530

    code updated

--- Main.java
+++ Main.java
@@ -1,244 +1,141 @@
-
 import java.util.Scanner;
-
 public class Main
 {
 
   static Scanner sc = new Scanner (System.in);
-
   static Node head = new Node (1, "1", 1);
-
   static dnode dhd = new dnode ("1", "1", "1", 1);
-
-
-
   static
   {
-
     Node temp = new Node (100, "A", 100);
-
-      head.next = temp;
-
+    head.next = temp;
   }
 
 
 
   static int updatecount = 0;
-
   public static void main (String[]args)
   {
-
-    /*{
-
-       Node temp= new Node(100,"A",100);
-
-       head.next=temp;
-
-       } */
-
     for (;;)
       {
-
-	System.out.println ("Menu\n1 Order product"
-			    + "\n2 See detail\n3 Update");
-
-	int choice = sc.nextInt ();
-
-	switch (choice)
-	  {
-
-	  case 1:
-	    order ();
-	    break;
-
-	    case 2:detail ();
-	    break;
-
-	    case 3:update ();
-	    break;
-
-	    default:System.out.println ("Invalid choice");
-
-	    break;
-
-	  }
+	      System.out.println ("Menu\n1 Order product\n2 See detail\n3 Update");
+        int choice = sc.nextInt ();
+    	switch (choice)
+	     {
+	      case 1:order ();  break;
+	      case 2:detail (); break;
+	      case 3:update (); break;
+	      default:System.out.println ("Invalid choice");
+        break;
+	     }
 
       }
 
   }
 
 
-
-
-
-  static void order ()
+  // THIS IS FOR ORDER 
+ public  static void order ()
   {
-
     Node temp = head.next;
-
     if (temp == null)
-      {
+      {System.out.println ("Sorry stock Empty"); return; }
 
-	System.out.println ("Sorry stock Empty");
-	return;
-
-      }
-
-    String name;
-    System.out.println ("Please Enter your name ");
-    name = sc.next();
-
-	System.out.println("Choose your product:\nName\tquanties\tprice\tName\tquanties\tprice");
-
+     String name;
+     System.out.println ("Please Enter your name ");
+     name = sc.next();
+    	System.out.println("Choose your product:\nName\tquanties\tprice\tName\tquanties\tprice");
+    
     while (temp != null)
       {
+	      System.out.print (temp.pnam + "\t" + temp.no + "\t\t" + "$" +temp.prc + "\t");
+        temp = temp.next;
+	      if(temp == null)
+	       break;
 
-	System.out.print (temp.pnam + "\t" + temp.no + "\t\t" + "$" +
-			  temp.prc + "\t");
-
-	temp = temp.next;
-
-	if (temp == null)
-	  break;
-
-	System.out.print (temp.pnam + "\t" + temp.no + "\t\t" + "$" +
-			  temp.prc);
-
-	temp = temp.next;
-
-	System.out.println ();
-
+	       System.out.print (temp.pnam + "\t" + temp.no + "\t\t" + "$" +temp.prc);
+         temp = temp.next;
+         System.out.println();
       }
 
     System.out.println ();
     System.out.println ("for cancellation Enter -1, any time");
-
     System.out.println ("Product name:");
 
     int nofp = 0;
-
     int price = 0;
-
     String pnam;
-
     String sp = " ";
 
     //search product
 
     while (true)
       {
-
 	pnam = sc.next ();
-
 	if (pnam.equals ("-1"))
-	  return;
-
+       return;
 	sp = sp + " , " + pnam;
-
 	temp = head;
-
 	while (temp != null)
 	  {
-
-	    if (pnam.equals (temp.pnam))
-	      {
-
+    if (pnam.equals (temp.pnam))
+      {
 		nofp = temp.no;
-
 		price = temp.prc;
-
 		break;
-
 	      }
-
-	    temp = temp.next;
-
+   temp = temp.next;
 	  }
 
 	if (temp != null)
 	  {
-
 	    break;
-
 	  }
 
 	System.out.println ("soory :( " + pnam + " is not in stock");
-
       }
-
-
-
     System.out.println ("Enter Quanty");
-
     int no;
-
     while (true)
       {
-
 	no = sc.nextInt ();
-
-	if (no == -1)
+ if (no == -1)
 	  return;
 
 	if (0 < no && no <= nofp)
 	  {
-
-	    break;
-
+    break;
 	  }
-
 	System.out.println ("please order less than or equals to " + nofp);
-
       }
 
 
 
 
 
-    System.out.println ("Just pay $" + no * price + " for " + no + " " +
-			pnam);
+   System.out.println ("Just pay $" + no * price + " for " + no + " " +pnam);
+   System.out.println ("If you want to buy type BUY");
+   String n = sc.next ();
 
-    System.out.println ("If you want to buy type BUY");
-
-    String n = sc.next ();
-
-
-
-    if (n.equals ("-1"))
+   if (n.equals ("-1"))
       return;
-
-
 
     if (!(n.equals ("BUY")))
       order ();
 
-
-
-
-
     temp = head;
-
     Node prv = head;
-
     while (temp != null)
-      {
-
+     {
 	prv = temp;
-
 	temp = temp.next;
-
 	if (pnam.equals (temp.pnam))
 	  {
-
 	    if (nofp == no)
 	      {
-
 		prv.next = temp.next;
-
 	      }
-
 	    temp.no = nofp - no;
-
 	    break;
 
 	  }
@@ -248,24 +145,19 @@ public class Main
     System.out.println ("Thank you " + name + ":) Visit again ");
 
 
-
 //storing history
-
-
-
     dnode tmp = new dnode (name, sp, pnam, no);
-
-
-
-    tmp.next = dhd.next;
+   tmp.next = dhd.next;
 
     dhd.next = tmp;
 
   }
+// END OF ORDER
 
 
 
-  static void detail ()
+  // this is function for details
+  public  static void detail ()
   {
    dnode tmp = dhd.next;
     if (tmp == null)
